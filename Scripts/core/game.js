0d892685f4e0f6f95607a1d239ccef2d4ded9f9a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-02-03 17:42:27 -0500

    added button event listeners and handlers

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -12,12 +12,24 @@
     function Start() {
         console.log("Starting Application...");
         stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20); // turn this on for buttons
         createjs.Ticker.framerate = 60; // 60 FPS
         createjs.Ticker.on("tick", Update);
         Main();
     }
     function Update() {
         stage.update(); // redraws the stage
+    }
+    function clickMeButtonMouseOver() {
+        clickMeButton.alpha = 0.7;
+    }
+    function clickMeButtonMouseOut() {
+        clickMeButton.alpha = 1.0;
+    }
+    function clickMeButtonClick() {
+        helloLabel.text = "Clicked!";
+        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
     }
     function Main() {
         console.log("Game Started...");
@@ -29,6 +41,9 @@
         clickMeButton.x = 320;
         clickMeButton.y = 340;
         stage.addChild(clickMeButton);
+        clickMeButton.on("mouseover", clickMeButtonMouseOver);
+        clickMeButton.on("mouseout", clickMeButtonMouseOut);
+        clickMeButton.on("click", clickMeButtonClick);
     }
     window.onload = Init;
 })();
